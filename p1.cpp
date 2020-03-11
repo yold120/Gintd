db05f4ac773944f7bfa537cc0fff7ee849241e21
yold120 <57795902+yold120@users.noreply.github.com>
2020-03-11 11:27:49 -0300

    Sistema con menu principal abierto

--- p1.cpp
+++ p1.cpp
@@ -22,7 +22,7 @@
 
 using namespace std;
 //a;adiendo la clase fecha
-
+	
 	void gotoxy(int x,int y) // funcion para localizar
 	{  
       HANDLE hcon;  
@@ -36,7 +36,7 @@ using namespace std;
      	x = 0;  
     	y = 0;
 	 }
-	  
+ 	  
 	};
 
 	void cleaner()  //funcion para borrar una parte de pantalla
@@ -66,22 +66,21 @@ using namespace std;
 	
 	void cuadro()	//almacen de localizacion de cuadros
 	{
-		bool pararcuad = true;
-		do 
-		{
+		
 		gotoxy(0,1);	//localizacion cuadro arriba
 		stylecu();
 	
 		gotoxy (0,20);	//localizacion cuadro abajo
 		stylecu();
-		}
-		while (pararcuad = true);
+
 	};
 	
 	void logogin()	//logo del programa
 	{
-		bool logo = true;
-		do{
+		
+		for (int logo = 0;logo<1;logo++)
+	{
+	
 		gotoxy(18,9);
 		cout << "**********************************************"<<endl;
 		Sleep (110);
@@ -107,17 +106,14 @@ using namespace std;
 		cout << "**********************************************"<<endl;
 		gotoxy(18,16);
 		cout << ""<<endl;	
-		cout<< endl;
+		cout<< endl; cout<<endl;
+		
+		cout<<"\t\t.........Presione una tecla para continuar.........";
 		cout<<endl;
-		system ("cls");
-		
-
-		}
-
-		while (logo = true);
-		
+		}	
 		
 	};
+	
 		
 	void fechaactual()	//fecha actual del sistema
 	{
@@ -126,27 +122,210 @@ using namespace std;
 	    current_time=time(NULL);
 	    ctime(&current_time);
 	    strcpy(fecha, ctime(&current_time));
-	    printf("%s", fecha);
-	
+	    printf("%s", fecha); 
 		
 	};
 	 
 	
 	void menuprincipalt()	//Titulo menu
 	{
-		gotoxy(28,9);
+		cuadro();	//invocando cuadro nuevamente
+		gotoxy(28,8);
 		cout << "*************************"<<endl;
-		gotoxy(28,10);
+		gotoxy(28,9);
 		cout << "*     MENU PRINCIPAL    *"<<endl;
-		gotoxy(28,11);
+		gotoxy(28,10);
 		cout << "*************************"<<endl;
 	};
 	
 	
+	void direccionales()
+	{
+		
+		
+		
+		//arriba = 22472 
+		//abajo = 22480
+		//izq = 22475
+		//der = 22477
+		/*system ("cls");
+		*/
+
+		int y=2;
+		char c;
+		gotoxy(0,y);
+		printf("-->");
+		gotoxy(5,1);
+		printf("Ingresar");
+		gotoxy(5,2);
+		printf("Informacion");
+		gotoxy(5,3);
+		printf("Salir");
+		
+		
+		/* menuprincipalt();
+		gotoxy (28,12);
+		cout<<"Iniciar seccion"<<endl;
+		gotoxy (28,14);
+		cout<<"Ingresar sin registrar"<<endl;
+		*/
+		/*do
+		{
+			c=getch();
+			if(c==-32)//para saber si es una flecha
+				{
+						
+					
+				}
+		}
+		
+		while (1);
+		*/
+		
+		
+		do 
+		{			
+			c=getch();
+			if (c==-32)//para saber si es direccional
+			{
+				
+
+				gotoxy(0,y);
+				printf("   ");
+				
+				c=getch();
+				if (c==72)//arriba
+					if (y==0)
+						{
+							y=3;	
+						}
+					else
+						{
+							 y--;
+								//y++;
+						}
+					
+					
+					if (c==80)//abajo
+							if (y==3)
+								{
+									y=1;
+								}
+							else
+								{
+									y++;
+								}
+								
+					gotoxy(0,y);
+					printf("-->");
+			}		
+		
+			if (c==13)
+				{
+				
+				gotoxy(20,10);
+				printf("escogistelaopcion %i",y);
+				
+				///// casos de menus
+				
+				do 
+				
+				{
+					switch(y) 
+					{
+					case 1: 
+						{
+							system ("cls");
+							gotoxy(28,12); printf ("Usuario:\n");
+							gotoxy(28,14);printf ("Clave:\n");
+							gotoxy(28,16);printf("salir:\n");
+							switch(y)
+							{
+							case 1: 
+							break;
+							case 2: 
+								
+							break;
+							case 3: gotoxy(28,16);printf("salir\n");
+							break;
+							default: printf("Error");
+							}
+							
+						}
+					break;
+					case 2: 
+					break;
+					case 3: 
+					break;
+					default: printf("Error");
+					}
+						
+				}
+				
+				while (y>5);
+					
+				}
+				
+			}
+			
+		 while (1);					
+		}
+		
+		
 	
+	//menu de opciones 1
+	/*	void opc1()
+	
+	{
+			
+
+	
+			int opcion;
+		    menuprincipalt();
+		    direccionales();
+		    do {
+		          // Para limpiar la pantalla
+		        
+		        // Texto del menú que se verá cada vez
+		        gotoxy(28,12);
+		        cout << "Ingresar" << endl;
+		        gotoxy(28,14);
+		        cout << "Acceder sin Registro" << endl;
+		        gotoxy(28,16);
+		        cout << "SALIR" << endl;
+		        cin >> opcion;
+		        
+		        switch (opcion) {
+		            case 1:
+		                // Lista de instrucciones de la opción 1                
+		                
+		                system("pause>nul"); // Pausa
+		                break;
+		                
+		            case 2:
+		                // Lista de instrucciones de la opción 2                
+		                
+		                system("pause>nul"); // Pausa
+		                break;
+		                
+		            case 3:
+		                // Lista de instrucciones de la opción 3                
+		                
+		                system("pause>nul"); // Pausa            
+		                break;
+		                
+		            case 4:
+		                // Lista de instrucciones de la opción 4                
+		                
+		                system("pause>nul"); // Pausa                
+		                break;
+		        }        
+		    } while (opcion != 5); 
+    
+	};
+	*/	
 int main()
 {
-	
 	bool clean = true;
 
 	//llamando almodo tama;o de ventana
@@ -154,21 +333,16 @@ int main()
 	//mostrando la fecha  pantalla
 	fechaactual();
 	//creandobarra griss de dise;o
-	
-	
-	
 	system ("cls");	//borrando 
-	logogin();	//invocando logo
-	Sleep (5000);
 
-	cuadro();	//invocando cuadros inicio
-	menuprincipalt();
+	logogin();	//invocando logo
+	getch();
+	system ("cls");
 	//llamando al logo de inicio
-	cuadro();	//invocando cuadro nuevamente
 	
+	//	opc1();
 	
-	
-	
+	direccionales();
 	system ("pause");
 	return 0;
 }
